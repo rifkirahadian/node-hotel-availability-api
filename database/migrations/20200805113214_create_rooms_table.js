529564703dde83249c13adcdac647f5120cb9d87
Rifki Rahadian <rrahadian94@gmail.com>
2020-08-05 13:27:49 +0700

    Migration-create: rooms, reservations

---
+++ database/migrations/20200805113214_create_rooms_table.js
@@ -0,0 +1,16 @@
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('rooms', function (table) {
+      table.increments('id')
+      table.string('room_number', 20).notNullable()
+      table.enum('room_status', ['available', 'out of service']).notNullable().defaultTo('available')
+      table.integer('hotel_id').unsigned().notNullable()
+      table.foreign('hotel_id').references('id').inTable('hotels')
+      table.timestamps()
+    })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('rooms')
+}
---
+++ database/migrations/20200805132114_create_reservations_table.js
@@ -0,0 +1,18 @@
+
+exports.up = function (knex) {
+  return knex.schema
+    .createTable('reservations', function (table) {
+      table.increments('id')
+      table.string('order_id', 20).notNullable()
+      table.string('customer_name').notNullable()
+      table.date('checkin_date').notNullable()
+      table.date('checkout_date').notNullable()
+      table.integer('hotel_id').unsigned().notNullable()
+      table.foreign('hotel_id').references('id').inTable('hotels')
+      table.timestamps()
+    })
+}
+
+exports.down = function (knex) {
+
+}
