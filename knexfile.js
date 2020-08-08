c31b436ed13a8ce1fa7a2298100c95fac188b010
Rifki Rahadian <rrahadian94@gmail.com>
2020-08-08 07:38:40 +0700

    Config: knexfile staging & production set

--- knexfile.js
+++ knexfile.js
@@ -13,34 +13,38 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: process.env.DB_SERVER,
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
     }
   },
 
   production: {
-    client: 'postgresql',
+    client: process.env.DB_SERVER,
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
     },
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './database/migrations'
     }
   }
 
